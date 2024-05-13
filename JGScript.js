5958d014f3062eefaaee5f78a913d8c39f124afe
47JMC <mjabir.5b@gmail.com>
2024-05-13 13:57:05 +0530

    Delete JGScript.js

--- JGScript.js
+++
@@ -1,5 +0,0 @@
-function SendUserToGame() {
-    let selectedGame = document.getElementById("GamesSelect").value;
-    let WhereToClick = "Click Me!"
-    document.getElementById("SelectedGame").innerHTML = "You selected: <a href='" + selectedGame + "' target='_blank'>" + WhereToClick + "</a>";
-}
