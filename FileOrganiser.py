2ebe5e4fa60eab62f4557bb7a0ca7d8f6638baa8
47JMC <mjabir.5b@gmail.com>
2024-05-20 16:40:10 +0000

    Updated Code of FileOrganiser.py

--- FileOrganiser.py
+++ FileOrganiser.py
@@ -1,24 +1,48 @@
-#========================
-#    File Organiser
-#========================
-#Libraries
-import os
-import shutil
+import os # Operating System Library
+import shutil # File Mover Library
 
-def Organise(home_folder, destination_folder):
-    files = os.listdir(home_folder) 
-    for file in files:
-        if file.endswith((".mp3", ".wav")):
-            shutil.move(os.path.join(home_folder, file), destination_folder)
-            print(f"Moved {file} to {destination_folder}")
+user = os.getlogin() # Get windows username
+default_dir = f"C:/Users/{user}/OneDrive/Desktop"
+
+not_whitelisted = [
+    "C:/Users/jarah/Downloads/",
+    "C:/Users/jarah/Downloads"
+]
+
+def Organise(home_folder_path, destination_folder_path: str, extension_endswith: str):
+    if home_folder_path not in not_whitelisted:
+        files = os.listdir(home_folder_path)
+        length = len(files)
+        moveable = 0
+        print(f"Found {length} Files")
+        for file in files:
+            if file.endswith(extension_endswith):
+                moveable = moveable +1
+                print(f"Moving {file} to {destination_folder_path}")
+                shutil.move(os.path.join(home_folder_path, file), destination_folder_path)
+        print(f"Targeted {moveable} files")
+
+    else:
+        print(f"{home_folder_path} is not whitelisted. Due to certain reasons with the windows file system")
+        exit()
 
 organiser_dir = input("Provide the path to the directory where the files you want to organise exist:")
-if organiser_dir == "" or organiser_dir == " ":
-    organiser_dir = f"C:/Users/{os.getlogin()}/Downloads/Organiser"
-print(organiser_dir)
 
-if os.path.exists(organiser_dir):
-    Organise(organiser_dir, f"C:/Users/{os.getlogin()}/Music/Playlists")
-else:
-    os.mkdir(organiser_dir)
-    print("Created Folder Organiser in ", organiser_dir)
+if organiser_dir == "":
+    print("Using Default Directory: Desktop")
+    organiser_dir = default_dir
+
+target_dir = input("Provide the path to the directory where the files will go to:")
+
+if target_dir == "":
+    print("Stopping Because of Required Option not filled")
+    exit()
+
+extension = input("Which files with the extension do you want to move (Example: .py .mp3)")
+
+if extension == "":
+    print("Stopping Because of Required Option not filled")
+    exit()
+
+Organise(organiser_dir, target_dir, extension)
+# Organise(organiser_dir, f"C:/Users/{user}/Music/Playlists", (".mp3",".wav"))
